120c807bc11d33f395cb9dda201496ff12a9616b
dave <>
2023-05-30 20:10:49 +0100

    refactor

--- manly_randomization_test/manly_randomization_test.py
+++ manly_randomization_test/manly_randomization_test.py
@@ -10,20 +10,21 @@ class RandomizationTest:
         self.combined = np.concatenate((group1, group2))
         self.iterations = int(iterations)
 
-    def observed_difference(self):
-        return np.mean(self.group1) - np.mean(self.group2)
+    @staticmethod
+    def calc_diff(group1, group2):
+        return abs(np.mean(group1) - np.mean(group2))
 
     def shuffle(self):
         np.random.shuffle(self.combined)
         return self.combined[: self.group1_n], self.combined[self.group1_n :]
 
     def test(self):
-        observed_difference = self.observed_difference()
+        actual_diff = self.calc_diff(self.group1,self.group2)
         count = 0
 
         for _ in range(self.iterations):
             group1, group2 = self.shuffle()
-            if abs(np.mean(group1) - np.mean(group2)) >= abs(observed_difference):
+            if self.calc_diff(group1,group2) >= actual_diff:
                 count += 1
 
         return count / self.iterations
